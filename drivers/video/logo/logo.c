8c0e60a9940a7113bc222cf658b2aa62091c787f
Kyosuke Nekomura <supercatexpert@gmail.com>
2018-09-03 16:41:46 +0900

    Added LOGO.

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -100,6 +100,10 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* M32R Linux logo */
 		logo = &logo_m32r_clut224;
 #endif
+#ifdef CONFIG_LOGO_NOVOTECH_CLUT224
+		/* Generic Linux logo */
+		logo = &logo_novotech_clut224;
+#endif
 	}
 	return logo;
 }
